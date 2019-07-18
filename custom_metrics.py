e2a445e45039130f222073e97b81c11a55a59c9c
tomruarol <tomruarol@gmail.com>
2019-07-18 16:50:27 +0200

    File Add/ custom_metrics.py

---
+++ custom_metrics.py
@@ -0,0 +1,50 @@
+'''
+Script to define Custom Metrics
+'''
+
+# imports 
+import os
+import time
+import datetime
+import json
+import gc
+from numba import jit
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm_notebook
+
+def fast_auc(y_true, y_prob):
+    """
+    fast roc_auc computation: https://www.kaggle.com/c/microsoft-malware-prediction/discussion/76013
+    """
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    nfalse = 0
+    auc = 0
+    n = len(y_true)
+    for i in range(n):
+        y_i = y_true[i]
+        nfalse += (1 - y_i)
+        auc += y_i * nfalse
+    auc /= (nfalse * (n - nfalse))
+    return auc
+
+
+def eval_auc(y_true, y_pred):
+    """
+    Fast auc eval function for lgb.
+    """
+    return 'auc', fast_auc(y_true, y_pred), True
+
+
+def group_mean_log_mae(y_true, y_pred, types, floor=1e-9):
+    """
+    Fast metric computation for this competition: https://www.kaggle.com/c/champs-scalar-coupling
+    Code is from this kernel: https://www.kaggle.com/uberkinder/efficient-metric
+    """
+    maes = (y_true-y_pred).abs().groupby(types).mean()
+    return np.log(maes.map(lambda x: max(x, floor))).mean()
